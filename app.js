b9fa58ffab06b77d4a9086951ea638296077dd12
RonnyK36 <ronnykelvyne3@gmail.com>
2022-08-10 13:26:06 +0300

    Word frequency calculator

--- app.js
+++ app.js
@@ -41,15 +41,35 @@
 // console.log(letterFrequency('Why is there a New Guy in town banaaaaa, kwanini'));
 
 //  sum numbers in an array
-const sumArrayNumbers = (numbers) => {
-    let result = 0;
-    for (const number of numbers) {
-        result += number;
+// const sumArrayNumbers = (numbers) => {
+//     let result = 0;
+//     for (const number of numbers) {
+//         result += number;
 
+//     }
+//     return { result }
+
+// }
+// const nums = [1, 3, 5, 2, 345, 56, 7];
+
+// console.log(sumArrayNumbers(nums));
+
+
+// Word Frequency in a Phrase
+
+const wordFrequency = (phrase) => {
+    wordArray = phrase.split(" ");
+    console.log(wordArray);
+    let frequency = {};
+
+    for (const word of wordArray) {
+        if (word in frequency) {
+            frequency[word] += 1
+        } else {
+            frequency[word] = 1
+        }
     }
-    return { result }
 
+    return frequency;
 }
-const nums = [1, 3, 5, 2, 345, 56, 7];
-
-console.log(sumArrayNumbers(nums));
+console.log(wordFrequency('Hii ni ya nini ni ya ninii ama ni ya nini'));
